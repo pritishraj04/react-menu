82132e44df6154399839a06218f43f762270449c
Pritish Raj <pritishraj04@gmail.com>
2021-04-08 20:40:07 +0530

    Updates

--- precache-manifest.4643e69c8379f18e6debc5c1eda41d90.js
+++ precache-manifest.4643e69c8379f18e6debc5c1eda41d90.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "71b484a39f4f772f6ab0f5c27f5c6f2d",
+    "revision": "42d767a8e1d9facdd918e382b845ad34",
     "url": "/react-menu/index.html"
   },
   {
-    "revision": "89c64490264e523c156a",
-    "url": "/react-menu/static/css/main.dace1448.chunk.css"
+    "revision": "e1e456187e7e867f7257",
+    "url": "/react-menu/static/css/main.dabe48a1.chunk.css"
   },
   {
     "revision": "139fbc5ac19195628142",
     "url": "/react-menu/static/js/2.5967e49d.chunk.js"
   },
   {
-    "revision": "89c64490264e523c156a",
-    "url": "/react-menu/static/js/main.f780bdb1.chunk.js"
+    "revision": "e1e456187e7e867f7257",
+    "url": "/react-menu/static/js/main.34d23fe3.chunk.js"
   },
   {
     "revision": "b0fcab4b47b9d3be89b6",
