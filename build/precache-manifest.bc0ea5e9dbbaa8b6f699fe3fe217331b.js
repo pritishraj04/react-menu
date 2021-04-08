7d59c28db7632e5a5d3eb75317b15617a0d30bef
Pritish Raj <pritishraj04@gmail.com>
2021-04-08 15:05:37 +0530

    gh-pages

---
+++ build/precache-manifest.bc0ea5e9dbbaa8b6f699fe3fe217331b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71b484a39f4f772f6ab0f5c27f5c6f2d",
+    "url": "/react-menu/index.html"
+  },
+  {
+    "revision": "89c64490264e523c156a",
+    "url": "/react-menu/static/css/main.dace1448.chunk.css"
+  },
+  {
+    "revision": "139fbc5ac19195628142",
+    "url": "/react-menu/static/js/2.5967e49d.chunk.js"
+  },
+  {
+    "revision": "89c64490264e523c156a",
+    "url": "/react-menu/static/js/main.f780bdb1.chunk.js"
+  },
+  {
+    "revision": "b0fcab4b47b9d3be89b6",
+    "url": "/react-menu/static/js/runtime~main.dd1e0f00.js"
+  }
+]);
